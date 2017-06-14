0816e696f41953febcc9325e3aff0630ed91abce
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-13 20:31:42 -0700

    added models and postman routes

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log("Unable to connect to DB: "+err);
+  }
+  db = db;
+  console.log('Connected to TodoApp Database');
+});
+
+module.exports = {mongoose}
---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+    console.log("Unable to save todo: "+err);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App now active on port ${port}`);
+});
