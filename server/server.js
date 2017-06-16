26c420db5c340fecd22d085dc9fc643e76d4c545
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-15 19:26:07 -0700

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const _ = require('lodash');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
@@ -66,6 +67,34 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+});
+
 
 app.listen(port, () => {
   console.log(`App now active on port ${port}`);
