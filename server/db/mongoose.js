aaae4c76f06ba0860d3ac8d0853ad01e3b4fb665
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-15 17:12:00 -0700

    set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log("Unable to connect to DB: "+err);
   }
