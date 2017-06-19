6bedd5e8b94f730b438282d20ea647d775855de5
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-18 17:05:50 -0700

    add GET /user/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 const {ObjectID} = require('mongodb');
 var app = express();
 
@@ -109,6 +110,7 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+
 /*
     ========    USERS    ========
 */
@@ -124,6 +126,12 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 
