ce9baa39d0002cc871ac4eb77752de4245edca18
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-19 19:49:54 -0700

    add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -146,6 +146,15 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.send(200);
+  }, () => {
+    res.status(400).send();
+  })
+});
+
 app.listen(port, () => {
   console.log(`App now active on port ${port}`);
 });
